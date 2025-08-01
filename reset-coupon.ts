8099d463a776722e33451faec26f46a1b1f27845
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2025-08-01 18:07:09 +0900

    chore: 쓸데없는거 지움

--- reset-coupon.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const reset = async () => {
-  try {
-    await axios.post('http://localhost:3000/coupons/reset');
-  } catch (e) {
-    console.error(e);
-  }
-};
